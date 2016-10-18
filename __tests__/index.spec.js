7da977d40f74139a4efdcf4b923917aa053f8216
NoHomey <ivo.stratev.tues@gmail.com>
2016-10-18 21:37:51 +0300

    making 100% code coverage

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,11 +1,33 @@
 "use strict";
 jest.mock('fs');
-jest.mock('constants');
 var index_1 = require('../index');
+var constants_1 = require('constants');
+var fs_1 = require('fs');
 describe('open-ioctl', function () {
     describe('devPath', function () {
         it('returns the concatination of /dev/ and the given path', function () {
             expect(index_1.devPath('test/test_dev')).toBe('/dev/test/test_dev');
+        });
+    });
+    describe('ioctlFlags', function () {
+        it('is the union of ioctl (3) and none blocking modes', function () {
+            expect(index_1.ioctlFlags).toEqual(3 | constants_1.O_NONBLOCK);
+        });
+    });
+    describe('openIoctl', function () {
+        it('opens a file based on /dev/ in ioctl, none blocking mode', function () {
+            function callback(err, fd) {
+                console.log(!err ? fd : err.message);
+            }
+            index_1.openIoctl('test/test_dev', callback);
+            expect(fs_1.open).toBeCalledWith('/dev/test/test_dev', 3 | constants_1.O_NONBLOCK, callback);
+        });
+    });
+    describe('openIoctlSync', function () {
+        it('opens synchroniosly a file based on /dev/ in ioctl, none blocking mode', function () {
+            fs_1.openSync.mockReturnValueOnce(9);
+            expect(index_1.openIoctlSync('test/test_dev')).toBe(9);
+            expect(fs_1.openSync).toBeCalledWith('/dev/test/test_dev', 3 | constants_1.O_NONBLOCK);
         });
     });
 });
--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,11 +1,37 @@
 jest.mock('fs');
-jest.mock('constants');
-import { devPath } from '../index';
+import { devPath, ioctlFlags, openIoctl, openIoctlSync } from '../index';
+import { O_NONBLOCK } from 'constants';
+import { open, openSync } from 'fs';
 
 describe('open-ioctl', () => {
     describe('devPath', () => {
         it('returns the concatination of /dev/ and the given path', () => {
             expect(devPath('test/test_dev')).toBe('/dev/test/test_dev');
+        });
+    });
+
+    describe('ioctlFlags', () => {
+        it('is the union of ioctl (3) and none blocking modes', () => {
+            expect(ioctlFlags).toEqual(3 | O_NONBLOCK);
+        });
+    });
+
+    describe('openIoctl', () => {
+        it('opens a file based on /dev/ in ioctl, none blocking mode', () => {
+            function callback(err: NodeJS.ErrnoException, fd: number): void {
+                console.log(!err ? fd : err.message);
+            }
+
+            openIoctl('test/test_dev', callback);
+            expect(open).toBeCalledWith('/dev/test/test_dev', 3 | O_NONBLOCK, callback);
+        });
+    });
+
+    describe('openIoctlSync', () => {
+        it('opens synchroniosly a file based on /dev/ in ioctl, none blocking mode', () => {
+            (openSync as jest.Mock<(path: string, mode: number) => number>).mockReturnValueOnce(9);
+            expect(openIoctlSync('test/test_dev')).toBe(9);
+            expect(openSync).toBeCalledWith('/dev/test/test_dev', 3 | O_NONBLOCK);
         });
     });
 });
